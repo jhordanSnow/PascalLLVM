b39481df1e787596c4829f1114c07de5b48a488d
jhordanSnow <jhordan.gt@gmail.com>
2018-06-14 00:04:27 -0600

    Main little change

--- interprete/main.cpp
+++ interprete/main.cpp
@@ -4,25 +4,38 @@
 #include<list>
 
 using namespace std;
+
+void reportError(string error){
+  std::cout << error << '\n';
+}
+
+list<string>* getInputStream(string fileName){
+  ifstream codefile (fileName);
+  string line;
+  list<string>* code = new list<string>();
+
+  if (codefile.is_open()) {
+     while (getline(codefile, line)) {
+        code->push_front(line);
+     }
+     codefile.close();
+  } else {
+     reportError("No se abrio ningun archivo");
+  }
+
+  return code;
+}
+
+
 int main(int argc, char** argv) {
    if (argc != 2) {
-      cout << "uso " << argv [0] << " <NombreArchivo>";
+      reportError("uso " + string(argv[0]) + " <NombreArchivo>");
       return -1;
    }
 
-   ifstream codefile (argv[1]);
-   string line;
-   list<string>* code = new list<string>();
-   if (codefile.is_open()) {
-      while (getline(codefile, line)) {
-         code->push_front(line);
-      }
-      codefile.close();
-   } else {
-      cout << "No se abrio ningun archivo";
-   }
+   list<string>* code = getInputStream(argv[1]);
 
-   cout << code->size() << endl;
+   std::cout << "caca - " << code->size() << '\n';
 
    return 0;
 }
