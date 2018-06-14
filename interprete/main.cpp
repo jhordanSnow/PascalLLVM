f5471d40fa746733064835ffa0e4b1324019427e
Ignacio Murillo <ignaciomurillo97@gmail.com>
2018-06-13 21:47:50 -0600

    imprimir de un archivo

--- interprete/main.cpp
+++ interprete/main.cpp
@@ -1,7 +1,25 @@
 #include<iostream>
+#include<fstream>
+#include<string>
 
 using namespace std;
-int main() {
-   cout << "caca" << endl;
+int main(int argc, char** argv) {
+   if (argc != 2) {
+      cout << "uso " << argv [0] << " <NombreArchivo>";
+      return -1;
+   }
+
+   ifstream codefile (argv[1]);
+   string line;
+   if (codefile.is_open()) {
+      while (getline(codefile, line)) {
+         cout << line << endl;
+      }
+      codefile.close();
+   } else {
+      cout << "No se abrio ningun archivo";
+   }
+
+
    return 0;
 }
