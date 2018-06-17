bf16a898fac7e26b411ae996e82614b2b19aa35e
JhordanSnow <jhordan.gt@gmail.com>
2018-06-16 22:05:45 -0600

    Token sin mierditas de value

--- interprete/main.cpp
+++ interprete/main.cpp
@@ -4,7 +4,6 @@
 #include<list>
 
 #include "headders/lexer.h"
-#include "headders/ast.h"
 
 using namespace std;
 using namespace MiniPascal;
@@ -30,44 +29,6 @@ list<string>* getInputStream(string fileName){
   return code;
 }
 
-Program* ArbolEjemplo () {
-   list<Identifier>* identifierList = new list<Identifier>();
-   identifierList->push_front(Identifier("a"));
-   identifierList->push_front(Identifier("b"));
-   identifierList->push_front(Identifier("c"));
-   identifierList->push_front(Identifier("d"));
-
-   list<VariableDeclaration>* variableDeclarationsList = new list<VariableDeclaration>();
-   variableDeclarationsList->push_front(VariableDeclaration(identifierList, new DataType(SimpleType::INTEGER)));
-
-   list<VariableNT>* variableListRead = new list<VariableNT>();
-   variableListRead->push_front(VariableNT(new EntireVariable(new VariableIdentifier(new Identifier("a")))));
-   variableListRead->push_front(VariableNT(new EntireVariable(new VariableIdentifier(new Identifier("b")))));
-   variableListRead->push_front(VariableNT(new EntireVariable(new VariableIdentifier(new Identifier("c")))));
-   variableListRead->push_front(VariableNT(new EntireVariable(new VariableIdentifier(new Identifier("d")))));
-
-   list<Statement>* statementList = new list<Statement>();
-   statementList->push_front(
-         Statement(
-            new SimpleStatement(
-               new ReadStatement(
-                     variableListRead
-                  )
-               )
-            )
-         );
-
-   Program* p = new Program(
-         new Identifier("ejemplo"),
-         new Block(
-            new VariableDeclarationPart(variableDeclarationsList),
-            new StatementPart(
-               new CompoundStatement(statementList)
-               )
-            )
-         );
-   return p;
-}
 
 int main(int argc, char** argv) {
    if (argc != 2) {
@@ -81,14 +42,11 @@ int main(int argc, char** argv) {
    list<Token*>* tokens = lexer->tokenize(code);
 
    for (list<Token*>::iterator it = tokens->begin(); it != tokens->end(); ++it){
-      Token* t = (*it);
-      std::cout << t->getTokenLocation() << ": " << t->getTokenName() << "\n";
+     Token* t = (*it);
+     std::cout << t->getTokenLocation() << ": " << t->getTokenName() << "\n";
    }
 
    std::cout << "Tokens - " << tokens->size() << '\n';
-
-   Program* p = ArbolEjemplo();
-   cout << "identificador" << p->identifier->identifier << endl;
 
    return 0;
 }
