a12b0cd5c8eb5764d770db73297dd90ea5243eb3
Ignacio Murillo <ignaciomurillo97@gmail.com>
2018-06-20 14:41:17 -0600

    poquito de codegen

--- compilador/test.pas
+++ compilador/test.pas
@@ -1,25 +1,10 @@
 program foo;
 // variables globales
-var i, j, b, c, d: integer; msg: char; caca: array [1..100] of char;
+var i, j, b, c, d: integer;
+caca : array [1..10] of integer;
 
-// compound statements: "main"
+//compound statements: "main"
 begin
   i := 0;
-  d := 1;
-  c := 0;
-  msg := 'Repeticiones ';
-  write(msg);
-  msg := '______________';
-  read(b);
-  while i < b do
-    begin
-      j := 0;
-      write(msg);
-      while j < b do
-        begin
-          if j = i then write(d) else write(c);
-          j := j + 1
-        end;
-      i := i+1
-    end
+  caca[0] := 0
 end.
